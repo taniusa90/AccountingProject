1a1e06fb87e85aae4310423e728f63142a388dd8
taniusa90 <galea_tanea@mail.com>
2023-01-25 17:02:45 -0600

    initial commit

---
+++ src/main/java/com/cydeo/javahedgehogsproject/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.cydeo.javahedgehogsproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(BaseEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
+    private Long insertUserId;
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
+    private Long lastUpdateUserId;
+    private Boolean isDeleted = false;
+
+}
