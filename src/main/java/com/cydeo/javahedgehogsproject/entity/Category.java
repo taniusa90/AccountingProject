c78c919423b9ab54ed2614c5f4b751482e5a44c9
taniusa90 <galea_tanea@mail.com>
2023-01-25 17:06:57 -0600

    initial commit

---
+++ src/main/java/com/cydeo/javahedgehogsproject/entity/Category.java
@@ -0,0 +1,22 @@
+package com.cydeo.javahedgehogsproject.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+@Where(clause = "is_deleted=false")
+public class Category  extends BaseEntity{
+
+    private String description;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+}
